2b318962783be0c41a0c1c015d65bd2b1b809ea1
Olegborodko <borodkoleg@gmail.com>
2019-12-19 01:08:24 +0200

    first day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getVideoId = require('get-video-id');
+const Axios = require('axios');
+const Convert = require('xml-js');
+
+
+class App extends Component {
+	inputChange = (data) => {
+		let obj = getVideoId(data.target.value)
+		if (obj.hasOwnProperty('id')){
+
+			Axios.get(`http://video.google.com/timedtext?lang=ru&v=${obj.id}` , {},
+    	{
+        headers: {
+            "Content-Type": "application/json"
+        },
+        params: {}
+	    })
+	    .then((res) => {
+	    	if (res['data']){
+	    		const objectWithText = Convert.xml2js(res['data'], {});
+
+	    		if (objectWithText.hasOwnProperty('elements')){
+	    			const arrayText = objectWithText['elements'][0]['elements']
+
+
+	    		}
+
+	    	} else {
+	    		console.log(res);
+	    	}
+	    })
+	    .catch(err => {
+	      console.log(err);
+	    });
+
+		}
+	}
+
+	render() {
+	  return (
+	    <div className="App" style={{textAlign: 'left', padding: '20px'}}>
+	      <input onChange={this.inputChange} />
+
+	      <ReactPlayer url='https://www.youtube.com/watch?v=bEwE4wyz00o' playing />
+	    </div>
+	  );
+	}
 }
 
 export default App;
